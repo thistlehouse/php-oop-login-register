32ad0750120a19838d1acb73b12214bb390777a4
felixtales <fernando2802@gmailcom>
2015-10-18 17:04:12 -0200

    implemented loggin

--- index.php
+++ index.php
@@ -6,3 +6,5 @@ if (Session::exists('home'))
 {
     echo '<p>' . Session::flash('home') . '</p>';
 }
+
+echo Session::get(Config::get('session.session_name'));
--- login.php
+++ login.php
@@ -1,3 +1,55 @@
 <?php
 
 require_once 'core/init.php';
+
+if (Input::exists())
+{
+    if (Token::check(Input::get('token')))
+    {
+        $validate = new Validate();
+        $validation = $validate->check($_POST, array(
+            'username' => array('required' => true),
+            'password' => array('required' => true),
+        ));
+
+        if ($validation->passed())
+        {
+            $user = new User();
+            $login = $user->login(Input::get('username'), Input::get('password'));
+
+            if ($login)
+            {
+                echo 'Success';
+            }
+            else
+            {
+                echo '<p>Sorry, loggin failed';
+            }
+
+        }
+        else
+        {
+            foreach ($validation->errors() as $error)
+            {
+                echo $error, '<br>';
+            }
+        }
+    }
+}
+?>
+
+<form action="" method="post">
+    <div class="field">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" autocomplete="off">
+    </div>
+
+    <div class="field">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
+    </div>
+
+    <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+    <input type="submit" value="Login">
+
+</form>
