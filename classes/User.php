23bbc7c3755e4e48d505e1b35278a8fe65b6dcef
Fernando Cardoso <fernando2802@gmail.com>
2015-10-18 17:50:32 -0200

    logout implemented

--- classes/User.php
+++ classes/User.php
@@ -78,6 +78,11 @@ class User
         return false;
     }
 
+    public function logout()
+    {
+        Session::delete($this->_sessionName);
+    }
+
     public function data()
     {
         return $this->_data;
