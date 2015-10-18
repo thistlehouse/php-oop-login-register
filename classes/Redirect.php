d0439e87998c4c7342250331aa151d158b900b7f
felixtales <fernando2802@gmailcom>
2015-10-18 03:05:26 -0200

    implemented redirect class

---
+++ classes/Redirect.php
@@ -0,0 +1,28 @@
+<?php
+
+class Redirect
+{
+    public static function to($location = null)
+    {
+        if ($location)
+        {
+            if (is_numeric($location))
+            {
+                switch (variable) {
+                    case 404:
+                        header('HTTP/1.0 404 Not Found');
+                        include 'includes/errors/404.php';
+                        break;
+
+                    default:
+                        # code...
+                        break;
+                }
+            }
+
+            header('Location: ' . $location);
+
+            exit();
+        }
+    }
+}
