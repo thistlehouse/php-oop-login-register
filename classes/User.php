2f34bdc0be6a47dc2a2e400065c3de8fb4ddc8ea
Fernando Cardoso <fernando2802@gmail.com>
2015-10-18 21:56:39 -0200

    implemented remembe me functionality

--- classes/User.php
+++ classes/User.php
@@ -5,6 +5,7 @@ class User
     private $_db,
             $_data,
             $_sessionName,
+            $_cookieName,
             $_isLoggedIn;
 
 
@@ -12,6 +13,7 @@ class User
     {
         $this->_db = DB::getInstance();
         $this->_sessionName = Config::get('session.session_name');
+        $this->_cookieName = Config::get('remember.cookie_name');
 
         if (!$user)
         {
@@ -25,7 +27,7 @@ class User
                 }
                 else
                 {
-
+                    // process Logout
                 }
             }
         }
@@ -61,26 +63,61 @@ class User
         return false;
     }
 
-    public function login($username = null, $password = null)
+    public function login($username = null, $password = null, $remember = false)
     {
-        $user = $this->find($username);
-
-        if ($user)
+        if (!$username && !$password && $this->exists())
         {
-            if ($this->data()->password === Hash::make($password, $this->data()->salt))
-            {
-                Session::put($this->_sessionName, $this->data()->id);
+            Session::put($this->_sessionName, $this->data()->id);
+        }
+        else
+        {
+            $user = $this->find($username);
 
-                return true;
+            if ($user)
+            {
+                if ($this->data()->password === Hash::make($password, $this->data()->salt))
+                {
+                    Session::put($this->_sessionName, $this->data()->id);
+
+                    if ($remember)
+                    {
+                        $hash       = Hash::unique();
+                        $hashCheck  = $this->_db->get('users_session', array('user_id', '=', $this->data()->id));
+
+                        if (!$hashCheck->count())
+                        {
+                            $this->_db->insert('users_session', array(
+                                'user_id'   => $this->data()->id,
+                                'hash'      => $hash,
+                            ));
+                        }
+                        else
+                        {
+                            $hash = $hashCheck->first()->hash;
+                        }
+
+                        Cookie::put($this->_cookieName, $hash, Config::get('remember.cookie_expiry'));
+                    }
+
+                    return true;
+                }
             }
         }
 
         return false;
     }
 
+    public function exists()
+    {
+        return (!empty($this->_data)) ? true : false;
+    }
+
     public function logout()
     {
+        $this->_db->delete('users_session', array('user_id', '=', $this->data()->id));
+
         Session::delete($this->_sessionName);
+        Cookie::delete($this->_cookieName);
     }
 
     public function data()
--- core/init.php
+++ core/init.php
@@ -26,3 +26,16 @@ spl_autoload_register(function($class) {
 });
 
 require_once 'functions/sanitize.php';
+
+if (Cookie::exists(Config::get('remember.cookie_name'))
+    && !Session::exists(Config::get('session.session_name')))
+{
+    $hash = Cookie::get(Config::get('remember.cookie_name'));
+    $hashCheck = DB::getInstance()->get('users_session', array('hash', '=', $hash));
+
+    if ($hashCheck->count())
+    {
+        $user = new User($hashCheck->first()->user_id);
+        $user->login();
+    }
+}
